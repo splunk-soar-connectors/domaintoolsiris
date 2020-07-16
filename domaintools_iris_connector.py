479d26f19c8fa5fb05342f8379715efa762ef760
cpangam <cpangam@splunk.com>
2020-07-16 15:49:37 -0700

    PAPP-10480: Bug fixes for Unicode values in the iput parameter

--- domaintools_iris_connector.py
+++ domaintools_iris_connector.py
@@ -13,12 +13,14 @@ from phantom.action_result import ActionResult
 
 # Imports local to this App
 
+import sys
 import json
 from datetime import datetime, timedelta
 import hmac
 import hashlib
 
 import requests
+from bs4 import UnicodeDammit
 
 
 # Define the App Class
@@ -47,7 +49,60 @@ class DomainToolsConnector(BaseConnector):
     def initialize(self):
         # get the app version
         self.app_version_number = self.get_app_json().get('app_version', '')
+
+        # Fetching the Python major version
+        try:
+            self._python_version = int(sys.version_info[0])
+        except:
+            return self.set_status(phantom.APP_ERROR,
+                                   "Error occurred while getting the Phantom server's Python major version.")
+
         return phantom.APP_SUCCESS
+
+    def _handle_py_ver_compat_for_input_str(self, input_str):
+        """
+        This method returns the encoded|original string based on the Python version.
+        :param python_version: Information of the Python version
+        :param input_str: Input string to be processed
+        :return: input_str (Processed input string based on following logic 'input_str - Python 3; encoded input_str - Python 2')
+        """
+
+        try:
+            if input_str and self._python_version == 2:
+                input_str = UnicodeDammit(input_str).unicode_markup.encode('utf-8')
+        except:
+            self.debug_print("Error occurred while handling python 2to3 compatibility for the input string")
+
+        return input_str
+
+    def _get_error_message_from_exception(self, e):
+        """ This function is used to get appropriate error message from the exception.
+        :param e: Exception object
+        :return: error message
+        """
+        error_msg = "Unknown error occurred. Please check the asset configuration and|or the action parameters."
+
+        try:
+            if e.args:
+                if len(e.args) > 1:
+                    error_code = e.args[0]
+                    error_msg = e.args[1]
+                elif len(e.args) == 1:
+                    error_code = "Error code unavailable"
+                    error_msg = e.args[0]
+            else:
+                error_code = "Error code unavailable"
+                error_msg = "Unknown error occurred. Please check the asset configuration and|or action parameters."
+        except:
+            error_code = "Error code unavailable"
+            error_msg = "Unknown error occurred. Please check the asset configuration and|or action parameters."
+
+        try:
+            error_msg = self._handle_py_ver_compat_for_input_str(error_msg)
+        except:
+            error_msg = "Unknown error occurred. Please check the asset configuration and|or action parameters."
+
+        return error_code, error_msg
 
     def _clean_empty_response(self, response):
         # PAPP-2087 DomainTools - Reverse Email table widget shows contextual action for no domain
@@ -84,8 +139,7 @@ class DomainToolsConnector(BaseConnector):
                                             error.get('message', 'Domain Tools failed to find IP/Domain'))
 
         if status == 503:
-            error_message = 'There was an error processing your request. Please try again or contact ' \
-                            '<a href=\"http://www.domaintools.com/support\">support</a> with questions'
+            error_message = error.get('message', "There was an error processing your request. Please try again or contact support (http://www.domaintools.com/support) with questions.")
             action_result.add_data({})
             return action_result.set_status(phantom.APP_ERROR, error_message)
 
@@ -149,13 +203,19 @@ class DomainToolsConnector(BaseConnector):
                 self.save_progress("GET: {}".format(url))
                 r = requests.get(url)
             except Exception as e:
-                return action_result.set_status(phantom.APP_ERROR, "REST API failed", e)
+                error_code, error_msg = self._get_error_message_from_exception(e)
+                if error_code == "ascii":
+                    error_msg = "Unicode value found. Please enter the valid input."
+                return action_result.set_status(phantom.APP_ERROR, "REST API failed.Error Code: {0}. Error Message: {1}".format(error_code, error_msg))
         else:
             try:
                 self.save_progress("POST: {} body: {}".format(url, data))
                 r = requests.post(url, data=data)
             except Exception as e:
-                return action_result.set_status(phantom.APP_ERROR, "REST API failed", e)
+                error_code, error_msg = self._get_error_message_from_exception(e)
+                if error_code == "ascii":
+                    error_msg = "Unicode value found. Please enter the valid input."
+                return action_result.set_status(phantom.APP_ERROR, "REST API failed.Error Code: {0}. Error Message: {1}".format(error_code, error_msg))
 
         self.save_progress("Parsing response...")
         try:
@@ -206,7 +266,9 @@ class DomainToolsConnector(BaseConnector):
         config = self.get_config()
 
         self._ssl = config.get('ssl', False)
-        self._username = config['username'].encode('utf-8')
+        self.save_progress("Username:", self._handle_py_ver_compat_for_input_str(config['username']))
+        self._username = self._handle_py_ver_compat_for_input_str(config['username'])
+        # self._username = config['username'].encode('utf-8')
         self._key = config['key'].encode('utf-8')
 
         if action_id == phantom.ACTION_ID_TEST_ASSET_CONNECTIVITY:
@@ -236,7 +298,7 @@ class DomainToolsConnector(BaseConnector):
 
     def _reverse_domain(self, param):
         action_result = self.add_action_result(ActionResult(param))
-        params = {'domain': param.get('domain')}
+        params = {'domain': self._handle_py_ver_compat_for_input_str(param.get('domain'))}
         ret_val = self._do_query('iris-investigate', action_result, data=params)
 
         if not ret_val:
@@ -269,7 +331,8 @@ class DomainToolsConnector(BaseConnector):
 
     def _domain_enrich(self, param):
         action_result = self.add_action_result(ActionResult(param))
-        params = {'domain': param.get('domain')}
+        domain_name = self._handle_py_ver_compat_for_input_str(param.get('domain'))
+        params = {'domain': domain_name}
         return self._do_domain_enrich(action_result, params)
 
     def _do_domain_enrich(self, action_result, params):
@@ -279,7 +342,7 @@ class DomainToolsConnector(BaseConnector):
     def _domain_reputation(self, param):
 
         action_result = self.add_action_result(ActionResult(param))
-        domain_to_query = param['domain']
+        domain_to_query = self._handle_py_ver_compat_for_input_str(param['domain'])
         params = {'domain': domain_to_query}
 
         ret_val = self._do_query('iris-investigate', action_result, data=params)
@@ -306,29 +369,29 @@ class DomainToolsConnector(BaseConnector):
         action_result = self.add_action_result(ActionResult(param))
 
         query_field = param['pivot_type']
-        query_value = param['query_value']
+        query_value = self._handle_py_ver_compat_for_input_str(param['query_value'])
 
         params = {query_field: query_value}
 
         if 'tld' in param:
-            params['tld'] = param['tld']
+            params['tld'] = self._handle_py_ver_compat_for_input_str(param['tld'])
 
         if 'data_updated_after' in param:
-            params['data_updated_after'] = param['data_updated_after']
+            params['data_updated_after'] = self._handle_py_ver_compat_for_input_str(param['data_updated_after'])
             if params['data_updated_after'].lower() == 'today':
                 params['data_updated_after'] = datetime.today().strftime('%Y-%m-%d')
             if params['data_updated_after'].lower() == 'yesterday':
                 params['data_updated_after'] = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
         if 'create_date' in param:
-            params['create_date'] = param['create_date']
+            params['create_date'] = self._handle_py_ver_compat_for_input_str(param['create_date'])
             if params['create_date'].lower() == 'today':
                 params['create_date'] = datetime.today().strftime('%Y-%m-%d')
             if params['create_date'].lower() == 'yesterday':
                 params['create_date'] = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
         if 'expiration_date' in param:
-            params['expiration_date'] = param['expiration_date']
+            params['expiration_date'] = self._handle_py_ver_compat_for_input_str(param['expiration_date'])
             if params['expiration_date'].lower() == 'today':
                 params['expiration_date'] = datetime.today().strftime('%Y-%m-%d')
             if params['expiration_date'].lower() == 'yesterday':
@@ -347,20 +410,66 @@ class DomainToolsConnector(BaseConnector):
 
 if __name__ == '__main__':
 
-    import sys
+    import pudb
+    import argparse
 
-    if (len(sys.argv) < 2):
-        print "No test json specified as input"
-        exit(0)
+    pudb.set_trace()
 
-    with open(sys.argv[1]) as f:
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('input_test_json', help='Input Test JSON file')
+    argparser.add_argument('-u', '--username', help='username', required=False)
+    argparser.add_argument('-p', '--password', help='password', required=False)
+
+    args = argparser.parse_args()
+    session_id = None
+
+    username = args.username
+    password = args.password
+
+    if (username is not None and password is None):
+        # User specified a username but not a password, so ask
+        import getpass
+
+        password = getpass.getpass("Password: ")
+
+    if (username and password):
+        try:
+            login_url = DomainToolsConnector._get_phantom_base_url() + '/login'
+
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=False)
+            csrftoken = r.cookies['csrftoken']
+
+            data = dict()
+            data['username'] = username
+            data['password'] = password
+            data['csrfmiddlewaretoken'] = csrftoken
+
+            headers = dict()
+            headers['Cookie'] = 'csrftoken=' + csrftoken
+            headers['Referer'] = login_url
+
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            session_id = r2.cookies['sessionid']
+        except Exception as e:
+            print("Unable to get session id from the platform. Error: " + str(e))
+            exit(1)
+
+    with open(args.input_test_json) as f:
         in_json = f.read()
         in_json = json.loads(in_json)
         print(json.dumps(in_json, indent=4))
 
         connector = DomainToolsConnector()
         connector.print_progress_message = True
+
+        if (session_id is not None):
+            in_json['user_session_token'] = session_id
+            connector._set_csrf_info(csrftoken, headers['Referer'])
+
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print (json.dumps(json.loads(ret_val), indent=4))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
